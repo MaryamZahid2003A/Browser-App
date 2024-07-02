08955067fa8338825f9fb96b6b7c75e8736f5c45
Maryam Zahid <maryamzahid960@gmail.com>
2024-07-02 03:00:50 -0700

    commit

---
+++ browse/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <p className='text-7xl text-center text-green-200'>Hello world</p>
+    </div>
+  );
+}
+
+export default App;
